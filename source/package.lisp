fe1707dcdaba9a08c0bbf13a2ca47b2243a3a41d
Artyom Bologov <aartaka@protonmail.com>
2023-09-09 19:26:40 +0400

    Add reset-toplevel-prompt to restore the previous prompt state.

--- source/package.lisp
+++ source/package.lisp
@@ -3,8 +3,11 @@
 
 (uiop:define-package :trivial-toplevel-prompt
   (:use :common-lisp)
-  (:export #:set-toplevel-prompt)
+  (:export #:set-toplevel-prompt #:reset-toplevel-prompt)
   (:documentation "`trivial-toplevel-prompt' is a library to customize REPL prompts.
 
 The main entry point is `set-toplevel-prompt', which redefines
-implementation-specific prompts to follow a certain format."))
+implementation-specific prompts to follow a certain format.
+
+`reset-toplevel-prompt' is an undo operation that restored the prompt
+to the state before the last `set-toplevel-prompt' was invoked."))
--- source/trivial-toplevel-prompt.lisp
+++ source/trivial-toplevel-prompt.lisp
@@ -3,6 +3,37 @@
 
 (in-package :trivial-toplevel-prompt)
 
+(defvar *previous-prompting-stack* nil
+  "A list of previous prompting states: replaced functions, rebound variables etc.
+Allows to restore the previous state of the prompt in `reset-toplevel-prompt'.")
+
+(defun reset-toplevel-prompt ()
+  "Return the prompt to the state before the last `set-toplevel-prompt'."
+  (if *previous-prompting-stack*
+      #+sbcl
+      (setf sb-int:*repl-prompt-fun*
+            (pop *previous-prompting-stack*))
+      #+ccl
+      (setf (fdefinition 'ccl::print-listener-prompt)
+            (pop *previous-prompting-stack*))
+      #+ecl
+      (setf si::*tpl-prompt-hook* (pop *previous-prompting-stack*))
+      #+clisp
+      (destructuring-bind (start body finish break step)
+          (pop *previous-prompting-stack*)
+        (setf sys::*prompt-start* start
+              sys::*prompt-body* body
+              sys::*prompt-finish* finish
+              sys::*prompt-break* break
+              sys::*prompt-step* step))
+      #+abcl
+      (setf *repl-prompt-fun* (pop *previous-prompting-stack*))
+      #+allegro
+      #-(or sbcl ccl ecl clisp abcl allegro)
+      nil
+      (warn "Nothing to reset: no previous state saved."))
+  nil)
+
 (defun shortest-package-nickname (package)
   (first (sort (cons (package-name package) (package-nicknames package))
                #'< :key #'length)))
@@ -38,17 +69,21 @@ The arguments for format control or function are:
             (function prompt-specifier))))
     (declare (ignorable prompt-function))
     #+sbcl
-    (setf sb-int:*repl-prompt-fun*
-          (lambda (stream)
-            (fresh-line stream)
-            (funcall prompt-function stream
-                     (sb-thread:thread-name sb-thread:*current-thread*)
-                     (shortest-package-nickname *package*)
-                     (incf *command-index*)
-                     (plusp sb-debug::*debug-command-level*) sb-debug::*debug-command-level*
-                     (sb-impl::stepping-enabled-p) (not (not (ignore-errors sb-ext::*inspected*))))))
+    (progn
+      (push sb-int:*repl-prompt-fun* *previous-prompting-stack*)
+      (setf sb-int:*repl-prompt-fun*
+            (lambda (stream)
+              (fresh-line stream)
+              (funcall prompt-function stream
+                       (sb-thread:thread-name sb-thread:*current-thread*)
+                       (shortest-package-nickname *package*)
+                       (incf *command-index*)
+                       (plusp sb-debug::*debug-command-level*) sb-debug::*debug-command-level*
+                       (sb-impl::stepping-enabled-p) (not (not (ignore-errors sb-ext::*inspected*)))))))
     #+ccl
     (progn
+      (push (fdefinition 'ccl::print-listener-prompt)
+            *previous-prompting-stack*)
       (fmakunbound 'ccl::print-listener-prompt)
       (setf (fdefinition 'ccl::print-listener-prompt)
             (lambda (stream &optional force)
@@ -65,29 +100,36 @@ The arguments for format control or function are:
                        ccl::@)
               (force-output stream))))
     #+ecl
-    (setf si::*tpl-prompt-hook*
-          (lambda ()
-            (fresh-line *standard-output*)
-            (funcall prompt-function *standard-output*
-                     (or #+threads (string (mp:process-name mp:*current-process*)) "REPL")
-                     (shortest-package-nickname *package*)
-                     (incf *command-index*)
-                     (plusp si::*tpl-level*) si::*tpl-level*
-                     (plusp si:*step-level*) (plusp si::*inspect-level*))
-            (force-output *standard-output*)))
+    (progn
+      (push si::*tpl-prompt-hook* *previous-prompting-stack*)
+      (setf si::*tpl-prompt-hook*
+            (lambda ()
+              (fresh-line *standard-output*)
+              (funcall prompt-function *standard-output*
+                       (or #+threads (string (mp:process-name mp:*current-process*)) "REPL")
+                       (shortest-package-nickname *package*)
+                       (incf *command-index*)
+                       (plusp si::*tpl-level*) si::*tpl-level*
+                       (plusp si:*step-level*) (plusp si::*inspect-level*))
+              (force-output *standard-output*))))
     #+abcl
-    (setf *repl-prompt-fun*
-          (lambda (stream)
-            (fresh-line stream)
-            (funcall prompt-function stream
-                     (threads:thread-name (threads:current-thread))
-                     (shortest-package-nickname *package*)
-                     tpl::*cmd-number*
-                     (plusp ext:*debug-level*) ext:*debug-level*
-                     ;; ABCL has no stepping?
-                     nil sys::*inspect-break*)))
+    (progn
+      (push *repl-prompt-fun* *previous-prompting-stack*)
+      (setf *repl-prompt-fun*
+            (lambda (stream)
+              (fresh-line stream)
+              (funcall prompt-function stream
+                       (threads:thread-name (threads:current-thread))
+                       (shortest-package-nickname *package*)
+                       tpl::*cmd-number*
+                       (plusp ext:*debug-level*) ext:*debug-level*
+                       ;; ABCL has no stepping?
+                       nil sys::*inspect-break*))))
     #+clisp
     (progn
+      (push (list sys::*prompt-start* sys::*prompt-body* sys::*prompt-finish*
+                  sys::*prompt-break* sys::*prompt-step*)
+            *previous-prompting-stack*)
       (setf sys::*prompt-start* ""
             sys::*prompt-break* ""
             sys::*prompt-step* ""
